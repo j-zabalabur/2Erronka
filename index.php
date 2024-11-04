16fd1d4a4ec65b4d772cc3eb24d2256b50408821
RobertRosu <i_rosu@fpzornotza.com>
2024-11-04 10:37:51 +0100

    Importazioak zuzenduta

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
-require_once("vista/header.php");
-require_once("vista/footer.php");
-require_once("vista/main.php");
+require_once("vista/ElementuKomunak.php");
+require_once("vista/hasieraOrria.php");
 
 ElementuKomunak::htmlHead();
 ElementuKomunak::htmlHeader();
@@ -10,7 +9,7 @@ $controller = isset($_GET['controller']) ? $_GET['controller'] : null;
 $action = isset($_GET['action']) ? $_GET['action'] : 'hasiera';
 
     if ($controller==null){
-        main::hasierakoOrria();
+        hasieraOrria::hasierakoOrria();
 
     } else {
         require_once("controlador/".$controller."Controller.php");

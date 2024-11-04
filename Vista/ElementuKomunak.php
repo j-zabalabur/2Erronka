dcd209772ac9d750afe244293692aab02b1397fa
RobertRosu <i_rosu@fpzornotza.com>
2024-11-04 13:15:03 +0100

    aldaketak htmlHead metodoan

--- Vista/ElementuKomunak.php
+++ Vista/ElementuKomunak.php
@@ -1,22 +1,24 @@
 <?php
 class ElementuKomunak {
 
-public static function htmlHead(){?>
-    <!DOCTYPE html>
-        <html lang="eu">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Sneakify - Marka onenen oinetakoak prezio onenean</title>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
-            <style>
-            <?php require_once("views/css/global.css");?>
-            <?php require_once("views/css/admin.css");?>
-            </style>
-        </head>
-        <body>
-    <?php
-}
+    public static function htmlHead(){?>
+        <!DOCTYPE html>
+            <html lang="eu">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Sneakify - Marka onenen oinetakoak prezio onenean</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+                <link rel="preconnect" href="https://fonts.googleapis.com">
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+                <link rel="stylesheet" href="vista/Css/global.css">
+            </head>
+            <body>
+        <?php
+    }
+    
 
 public static function htmlHeader(){?>
     <header>

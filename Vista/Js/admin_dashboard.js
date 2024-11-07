6318d2c305a63521204c4e05457b18c8dc8a4498
Jon Zabala <j_zabalabur@fpzornotza.com>
2024-11-07 09:01:58 +0100

    Deskontu kodeak ikusteko funtzionalitatea

--- Vista/Js/admin_dashboard.js
+++ Vista/Js/admin_dashboard.js
@@ -31,6 +31,27 @@
 
 
 
+        async function deskontuKodeakIkusi(){
+            await fetch("http://localhost/ariketak/2Erronka/Controlador/DeskontuKodeakIkusi.php")
+                .then(response => response.json())
+                .then(data => {
+                    document.getElementById('emaitzaDeskontuKodeak').innerHTML = "";
+                    data.forEach(item => {
+                        ilara = `
+                            <tr>
+                                <td>${item.kodea}</td>
+                                <td>${item.deskontua}</td>
+                                <td>
+                                    <button type='button' class='btn btn-info' onclick=aldatu('${item.kodea}')>Aldatu</button>
+                                    <button type='button' class='btn btn-danger' onclick=ezabatu('${item.kodea}')>Ezabatu</button>
+                                </td>
+                            </tr>
+                        `;
+                        console.log(ilara);
+                        document.getElementById('emaitzaDeskontuKodeak').innerHTML+= ilara;
+                    })
+                })
+        }
 
 
 

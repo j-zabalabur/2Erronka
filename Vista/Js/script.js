f0077551ce9834cbab6abf427400ae8e4f866c6f
RobertRosu <ioanrobertrosu58@gmail.com>
2024-11-07 17:25:05 +0100

    eventListenerra fitxategitik kendu da

--- Vista/Js/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('header').addEventListener('scroll', function(){
-    if(window.scrollY > 15){
-        document.querySelector('header').classList.add('scroll')
-    }else{
-        document.querySelector('header').classList.remove('scroll')
-    }
-})

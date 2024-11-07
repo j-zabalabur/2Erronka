ffc00cbaea117ff6e127e796688323270fd48aad
Jon Zabala <j_zabalabur@fpzornotza.com>
2024-11-07 08:49:29 +0100

    Akatsa zuzendu

--- Modelo/Erabiltzailea.php
+++ Modelo/Erabiltzailea.php
@@ -2,18 +2,6 @@
 include_once('Konexioa.php');
 
 class Erabiltzailea extends Konexioa{
-    public function getErabiltzaileak(){
-        $query = $this->getCon()->query("SELECT * FROM erabiltzaileak");
-        $array = [];
 
-        if ($query){
-            while ($lerroa = $query->fetch_assoc()) {
-                $array[] = $lerroa;
-            }
-        }
-            return $array;
-
-
-    }
 }
 ?>
--- Vista/Js/admin_dashboard.js
+++ Vista/Js/admin_dashboard.js
@@ -33,62 +33,7 @@
 
 
 
-        async function deskontuKodeakIkusi(){
-            await fetch("http://localhost/ariketak/2Erronka/Controlador/DeskontuKodeakIkusi.php")
-                .then(response => response.json())
-                .then(data => {
-                    document.getElementById('emaitzaDeskontuKodeak').innerHTML = "";
-                    data.forEach(item => {
-                        ilara = `
-                            <tr>
-                                <td>${item.kodea}</td>
-                                <td>${item.deskontua}</td>
-                                <td>
-                                    <button type='button' class='btn btn-info' onclick=aldatu('${item.kodea}')>Aldatu</button>
-                                    <button type='button' class='btn btn-danger' onclick=ezabatu('${item.kodea}')>Ezabatu</button>
-                                </td>
-                            </tr>
-                        `;
-                        console.log(ilara);
-                        document.getElementById('emaitzaDeskontuKodeak').innerHTML+= ilara;
-                    })
-                })
-        }
 
-        async function erabiltzaileakIkusi(){
-            await fetch("http://localhost/ariketak/2Erronka/Controlador/ErabiltzaileakIkusi.php")
-                .then(response => response.json())
-                .then(data => {
-                    document.getElementById('emaitzaErabiltzaileak').innerHTML = "";
-                    data.forEach(item => {
-                        ilara = `
-                            <tr>
-                                <td>${item.id}</td>
-                                <td>${item.email}</td>
-                                <td>${item.izena}</td>
-                                <td>${item.abizena}</td>
-                                <td>`
-
-                        //Administratzailea da? TinyInt-etik Bai/Ez-era
-                        if (item.administratzailea==1){
-                            ilara += `Bai`
-                        } else {
-                            ilara += `Ez`
-                        }
-                                
-                        ilara +=`</td>
-                                <td>${item.helbidea}</td>
-                                <td>
-                                    <button type='button' class='btn btn-info' onclick=aldatu('${item.id}')>Aldatu</button>
-                                    <button type='button' class='btn btn-danger' onclick=ezabatu('${item.id}')>Ezabatu</button>
-                                </td>
-                            </tr>
-                        `;
-                        console.log(ilara);
-                        document.getElementById('emaitzaErabiltzaileak').innerHTML+= ilara;
-                    })
-                })
-        }
         
         async function eskaerakIkusi(){
             await fetch("http://localhost/ariketak/2Erronka/Controlador/EskaerakIkusi.php")

319eb596c535a3047afa81a6a491635657166cc6
RobertRosu <i_rosu@fpzornotza.com>
2024-11-11 10:34:57 +0100

    aldaketak scriptan

--- Vista/Js/produktua.js
+++ Vista/Js/produktua.js
@@ -4,13 +4,26 @@ function produktu_id_jaso(){
     return urlParams.get('id')
 }
 
-async function produktu_datuak_jaso(){
-    const id = produktu_id_jaso()
-    await fetch(`http://localhost/2Erronka/Controlador/ProduktuaIkusi.php?id=${id}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-    })
+async function fetch_data(url){
+    try{
+        const data = await fetch(url)
+        .then(res => {
+            if(res.ok){
+                return res.json()
+            }else{
+                throw new Error(`${res.status}: ${res.statusText}`)
+            }
+        })
+
+        return data
+    }catch(e){
+        return e
+    }
 }
 
-produktu_datuak_jaso()
+async function produktu_datuak_txertatu(){
+    const datuak = await fetch_data(`http://localhost/2Erronka/Controlador/ProduktuaIkusi.php?id=${produktu_id_jaso()}`)
+    console.log(datuak.izena)
+}
+
+produktu_datuak_txertatu()

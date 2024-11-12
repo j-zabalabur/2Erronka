d40133e1f064d648928f245ac7415fb975835916
RobertRosu <ioanrobertrosu58@gmail.com>
2024-11-12 14:50:16 +0100

    Funtzionalitate orokorrak fitxategin honetan egongo dira

---
+++ Vista/Js/script.js
@@ -0,0 +1,21 @@
+async function fetch_data(url) {
+    const data = await fetch(url)
+    return data.json()
+}
+
+async function erabiltzaile_datuak_txertatu(){
+    if(localStorage.getItem('id')){
+        const id = localStorage.getItem('id')
+        const datuak = await fetch_data(`http://localhost/2Erronka/Controlador/ErabiltzaileaDatuakJaso.php?id=${id}`)
+
+        document.getElementById('izen-abizenak').innerText = `${datuak.izena} ${datuak.abizena}`
+        document.getElementById('email').innerText = datuak.email
+        document.getElementById('orga-produktu-kopurua').innerText = datuak.orga_produktuak
+    }
+}
+
+function prezio_beheratua(prezioa, beherapena){
+    return Math.floor((prezioa - (prezioa * beherapena) / 100) * 100) / 100
+}
+
+document.addEventListener('DOMContentLoaded', erabiltzaile_datuak_txertatu)

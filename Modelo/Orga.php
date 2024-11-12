2aeafcfa44c36da2391b31cffabb7386da478a62
2AW3 User 2 <2AW3-2@yourdomain.com>
2024-11-12 13:10:01 +0100

    orga ikusi

--- Modelo/Orga.php
+++ Modelo/Orga.php
@@ -2,5 +2,19 @@
 include_once('Konexioa.php');
 
 class Orga extends Konexioa{
-    
+    public static function getOrga($idEra){
+        $konexioa = new Konexioa;
+        $query = $konexioa->getCon()->query("SELECT id_erabiltzailea,id_produktua,kopurua,produktuak.izena, produktuak.prezioa, produktuak.beherapena, produktuak.argazkia FROM orga_lerroak INNER JOIN produktuak ON orga_lerroak.id_produktua = produktuak.id WHERE orga_lerroak.id_erabiltzailea = $idEra");
+
+        $array = array();
+
+        if ($query){
+            while ($lerroa = $query->fetch_assoc()) {
+                $base64 = base64_encode($lerroa['argazkia']);
+                $lerroa['argazkia'] = $base64;
+                $array[] = $lerroa;
+            }
+        }
+        return $array;
+    }
 }

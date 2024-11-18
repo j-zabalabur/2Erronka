931c1471869318a4b81057a453f3b13a49c4f57c
Jon Zabala <j_zabalabur@fpzornotza.com>
2024-11-18 12:43:44 +0100

    URL dinamikoekin probak

--- Vista/Js/produktua.js
+++ Vista/Js/produktua.js
@@ -74,6 +74,9 @@ async function produktu_datuak_txertatu(){
 
     document.querySelector('#deskripzioa #tituloa').innerText = tituloa.toUpperCase()
     document.querySelector('#deskripzioa #deskripzio-gorputza').innerText = gorputza
+
+    const nuevaURL = `/produktuak/${datuak.eragina.toUpperCase()}-${datuak.izena}.html`;
+    history.pushState(null, null, nuevaURL);
 }
 
 document.addEventListener('DOMContentLoaded', produktu_datuak_txertatu)
@@ -92,3 +95,4 @@ document.getElementById('saioa').addEventListener('click', function(e){
         location.href = "saioaHasi.html"
     }
 }) 
+
